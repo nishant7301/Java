46ffa25e3233328c208eaa6fbc97efebd4b7ca63
Nishant <nishantraj2770@gmail.com>
2023-02-11 18:51:17 +0530

    print has been modified

---
+++ Java Basic/Java Number Program/Reverse.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Reverse
+{
+public static void main(String args[])
+    {
+ Scanner sc=new Scanner(System.in);
+ int n=sc.nextInt();
+ int sum=0;
+ while(n>0)
+ {
+ int r=n%10;
+ sum=sum*10+r;
+ n=n/10;
+ }
+ System.ou.println(sum);
+
+    }
+}
